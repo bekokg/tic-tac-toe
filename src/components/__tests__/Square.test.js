c5cadbd380b924b8ba03bc3af63cf7ae28c5ef4b
Bekten Orunbaev <beko_iuk@outlook.com>
2021-10-04 21:26:36 -0500

    test added for text X

--- src/components/__tests__/Square.test.js
+++ src/components/__tests__/Square.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import Square from '../Square';
 
-test('renders Board component', () => {
-  render(<Square />);
-  const btnElement = screen.getByTestId(/square/i);
-  expect(btnElement).toBeInTheDocument();
-});
+describe('Square component', ()=> {
+
+  test('renders Square component', () => {
+    render(<Square />);
+    const btnElement = screen.getByTestId(/square/i);
+    expect(btnElement).toBeInTheDocument();
+  });
+
+  test('renders Square component with prop', () => {
+    render(<Square type='X'/>);
+    const btnXElement = screen.getByText(/X/i);
+    expect(btnXElement).toBeInTheDocument();
+  });
+})
