17393fe0cd60dd2b5243fab6f7e4bc089c9eecc6
Bekten Orunbaev <beko_iuk@outlook.com>
2021-10-11 03:29:52 -0500

    Display Board component in App testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders tic tac toe app', () => {
-  render(<App />);
-  const divElement = screen.getByText(/Tic Tac Toe/i);
-  expect(divElement).toBeInTheDocument();
-});
+describe('App component', ()=> {
+
+  test('renders tic tac toe app', () => {
+    render(<App />);
+    const divElement = screen.getByText(/Tic Tac Toe/i);
+    expect(divElement).toBeInTheDocument();
+  });
+
+  test('renders tic tac toe app with Board component', () => {
+    render(<App />);
+    const divElement = screen.getByTestId(/board/i);
+    expect(divElement).toBeInTheDocument();
+  });
+})
