b823c72fa03e25a5bfa27b8d21d98dabdf4745d7
Bekten Orunbaev <beko_iuk@outlook.com>
2021-10-11 09:38:27 -0500

    O winner test

--- src/components/__tests__/calculateWinner.test.js
+++ src/components/__tests__/calculateWinner.test.js
@@ -4,4 +4,9 @@ describe('Game calculator', ()=> {
   it('should return X as winner', ()=> {
     expect(calculateWinner(['X', 'O', 'O', 'X', 'X', 'X', 'O', 'X', 'O'])).toEqual('X')
   });
+
+  it('should return O as winner', ()=> {
+    expect(calculateWinner(['O', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O'])).toEqual('O')
+  })
+ 
 })
