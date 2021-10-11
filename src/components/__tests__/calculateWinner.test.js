e65054439c646bcf45e40dd657690e977eb4a64c
Bekten Orunbaev <beko_iuk@outlook.com>
2021-10-11 09:39:37 -0500

    No winner test add for calculateWinner method

--- src/components/__tests__/calculateWinner.test.js
+++ src/components/__tests__/calculateWinner.test.js
@@ -8,5 +8,8 @@ describe('Game calculator', ()=> {
   it('should return O as winner', ()=> {
     expect(calculateWinner(['O', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O'])).toEqual('O')
   })
- 
+
+  it('should return null if no winner', ()=> {
+    expect(calculateWinner([null, null, null, null, null, null, null, null, null])).toEqual(null)
+  })
 })
