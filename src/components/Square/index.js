b3e4b6499410950ccef702fb7ecde61b46f53f8d
Bekten Orunbaev <beko_iuk@outlook.com>
2021-10-12 23:08:39 -0500

    square with value disable add

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Square({type, onClick, index}) {
   return (
-    <button className={`squares ${type || ''}`} onClick={onClick} data-testid={`square-${index}`}>
+    <button className={`squares ${type || ''}`} onClick={onClick} disabled={type ? true : false} data-testid={`square-${index}`}>
       {type}
     </button>
   );
